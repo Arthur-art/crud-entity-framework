145f1705633541ef1aa1e09e41ec304b23f38ab2
arthur-art <arthurteixeira.guts@gmail.com>
2023-04-12 21:36:54 -0300

    Gerando a primeira migration, criando banco de dados no sql express local

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddDbContext<NoteBookContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Notebook")));
+builder.Services.AddDbContext<NoteBookContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("database")));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # crud-entity-framework
+
+### Trabalhando com migrations
+- Adicionando connection string com sql-express no banco local:
+https://learn.microsoft.com/pt-br/ef/core/dbcontext-configuration/
+
+- Gerando migration e atualizando no banco de dados:
+https://learn.microsoft.com/pt-br/ef/core/cli/dotnet
+
+https://learn.microsoft.com/pt-br/ef/core/managing-schemas/migrations/?tabs=dotnet-core-cli
